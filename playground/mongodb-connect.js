8c306b4fa25001abc670d8afb225eef53243b9b3
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-09-12 21:14:19 +0200

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('Error connecting to MongoDb server')
+    }
+    console.log('Connected to MongoDb server ...');
+
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'Insert one',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('unable to insert todo document', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
+});
