cc1eda6aa686d9354c156cc7b61446f8a93e6218
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-09-25 22:24:39 +0200

    impl post api

--- server/server.js
+++ server/server.js
@@ -8,13 +8,22 @@ let app = express();
 app.use(express.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
-    console.log('server is up on port 3000');
+    console.log('Started on port 3000');
 });
+
+module.exports = {app};
 
 
 
