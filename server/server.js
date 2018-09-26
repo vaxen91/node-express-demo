05553ab9f48748150c5267d9c403e0d730aae44a
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-09-26 22:50:42 +0200

    minor fix

--- server/server.js
+++ server/server.js
@@ -19,54 +19,10 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
-});
+if (!module.parent) {
+    app.listen(3000, () => {
+        console.log('Started on port 3000');
+    });
+}
 
 module.exports = {app};
-
-
-
-
-
-
-
-// let newTodo = new Todo({text: 'Cucinare la cena'});
-//
-// newTodo.save().then((response) => {
-//         console.log('Saved todo ' + response);
-//     },
-//     (error) => {
-//         console.log(error)
-//     });
-//
-// let otherTodo = new Todo({text: 'Programmare'});
-//
-// otherTodo.save().then((response) => {
-//         console.log('Saved todo ' + response);
-//     },
-//     (error) => {
-//         console.log(error)
-//     });
-//
-// let newUser = new User({username: 'Matt',email:'demo@demo', password: 'ddd'});
-//
-// newUser.save().then((response) => {
-//         console.log('Saved user ' + response);
-//     },
-//     (error) => {
-//         console.log(error)
-//     });
-//
-// app.use(express.static(__dirname + '/public'));
-// // respond with "hello world" when a GET request is made to the homepage
-
-// // respond with a random err message
-// app.get('/bad', (req, res) => {
-//     res.send({
-//         errCode: 404,
-//         errMessage: 'NOT FOUND'
-//     })
-// });
-//
-
