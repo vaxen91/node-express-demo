4bdf0ff8ce8c4f91b31dbd158ef9910f84edc6c6
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-09-26 23:22:01 +0200

    impl get todos

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ let app = express();
 app.use(express.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
+    let todo = new Todo({
         text: req.body.text
     });
 
@@ -19,10 +19,20 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 if (!module.parent) {
     app.listen(3000, () => {
         console.log('Started on port 3000');
     });
 }
 
-module.exports = {app};
+module.exports = {app : app};
