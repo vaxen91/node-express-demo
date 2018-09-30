430fbbed00e82488bbe2dfdfc825644ca82caf20
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-10-01 00:52:07 +0200

    impl get/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
@@ -29,10 +29,27 @@ app.get('/todos', (req, res) => {
     })
 });
 
+app.get('/todos/:id', (req, res) => {
+    id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send(e.body));
+});
+
 if (!module.parent) {
     app.listen(3000, () => {
         console.log('Started on port 3000');
     });
 }
 
-module.exports = {app : app};
+module.exports = {
+    app: app
+};
